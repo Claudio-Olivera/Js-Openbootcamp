0369f111c99bd38c87428bcb9af1074058db4fb9
claudio-olivera <claudioolivera753@gmail.com>
2022-08-26 12:55:42 -0300

    2.Ejercicio lista, objetos, fechas

---
+++ 2 Sintaxis, variables y palabras reservadas/index.js
@@ -0,0 +1,12 @@
+const libroFavorito={
+    titulo:"Misery",
+    autor:"Stephen King",
+    fecha:1987,
+    url:"https://www.amazon.com/Misery-Stephen-King-audiobook/dp/B019FQXLLO/ref=sr_1_1?keywords=misery+stephen+king&qid=1661528217&s=books&sprefix=misery+%2Cstripbooks-intl-ship%2C273&sr=1-1",
+}
+const fechaNacimiento = new Date(1989,05,12);
+
+let datos=["claudio", 33, true, fechaNacimiento, libroFavorito]
+
+console.log(datos[0],datos[1],datos[2],datos[3],datos[4]); 
+//no entiendo porque quoka me dice junio como respuesta al mes may 05
