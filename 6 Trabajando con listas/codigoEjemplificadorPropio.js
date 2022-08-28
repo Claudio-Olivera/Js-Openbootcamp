a20d5a1f74459e65dde04fc0cb6201be5c303598
claudio-olivera <claudioolivera753@gmail.com>
2022-08-28 16:43:24 -0300

    6. map filter ejemplo propio sobre objetos, sort

--- 6 Trabajando con listas/codigoEjemplificadorPropio.js
+++ 6 Trabajando con listas/codigoEjemplificadorPropio.js
@@ -60,3 +60,33 @@ personitas.filter(persona => {
         console.log(persona.nombre) ;
     }
 })
+
+const personalidades = [
+    {
+        nombre:"Luis",
+        edad: 20,
+        email: "luis928@gmail.com.ar"
+    },
+    {
+        nombre:"Hernan",
+        edad: 22,
+        email: "Hernan928@gmail.com.ar"
+    },
+    {
+        nombre:"Patricia",
+        edad: 19,
+        email: "pato28@gmail.com.ar"
+    },
+    {
+        nombre:"Micaela",
+        edad: 19,
+        email: "mica28@gmail.com.ar"
+    },
+]
+/*ordenar los datos de menor a mayor , la inversa seria b - a */
+/*OJO sort si modifica los datos de personalidades por eso no es necesario asignarle
+otra variable*/
+/*sort a es elemento anterior , y b seria elemento siguiente , sort puede retornar positivo, negativo y cero
+si pasa negativo significa que a tiene indicice menor que b , aplicando un return -1 nos devolveria toda la lista al reves.*/
+personalidades.sort((a, b) => a.edad - b.edad)
+console.log(personalidades)
