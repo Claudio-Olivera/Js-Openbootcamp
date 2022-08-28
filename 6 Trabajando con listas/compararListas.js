ba20ee1f1649db5680bc584856e2e39d7df4816c
claudio-olivera <claudioolivera753@gmail.com>
2022-08-28 17:31:28 -0300

    6. every comparar listas

--- 6 Trabajando con listas/compararListas.js
+++ 6 Trabajando con listas/compararListas.js
@@ -22,6 +22,65 @@ console.log(compararArr(arr1,arr3))
 const arr4 = [1,2,3,4,6]/*pasa la primera comprobacion pero no la segunda(every)*/
 console.log(compararArr(arr1,arr4))
 
-/*every devolvera true o false dependiendo de las condiciones
+/*every devolvera true o false si los valores cumplen la condicion
 OJO, SI LO PASO VACIO DEVUELVE SIEMPRE TRUE
 */
+
+/*some devuelve true si al menos uno de los valores cumple la condicion*/
+const arr8 = [1,2,3,4,5,-8]
+
+/** hay algun valor menor a 0 ?*/
+const res = arr8.some (valor => valor < 0)
+console.log(res)
+/** hay algun valor menor a -20 ?*/
+const res2 = arr8.some (valor => valor < -20)
+console.log(res2)
+/** hay algun valor igual a 5 */
+const existe = arr8.some(valor => valor === 5)
+console.log(existe)
+
+/* Ejemplo con objetos*/
+const personas = [
+    {
+        nombre:"Luis",
+        edad: 20,
+        email: "luis928@gmail.com.ar"
+    },
+    {
+        nombre:"Hernan",
+        edad: 22,
+        email: "Hernan928@gmail.com.ar"
+    },
+    {
+        nombre:"Patricia",
+        edad: 19,
+        email: "pato28@gmail.com.ar"
+    },
+    {
+        nombre:"Micaela",
+        edad: 19,
+        email: "mica28@gmail.com.ar"
+    },
+]
+
+/*existe micaela en personas ?*/
+const existeMicaela = personas.some(persona => persona.nombre === "Micaela")
+/*existe = true*/
+console.log(existeMicaela)
+
+// Cómo obtener una lista a partir de un objeto iterable
+const str = "Hola soy Gorka"
+console.log(str[5])
+
+const ar_str = Array.from(str)
+console.log(ar_str)
+
+const set = new Set([2, 3, "hola", 4])
+const ar_set = Array.from(set)
+console.log(ar_set)
+
+const keys = ar_set.keys()
+console.log(keys)
+
+const ar_keys = Array.from(keys)
+console.log(ar_keys)
