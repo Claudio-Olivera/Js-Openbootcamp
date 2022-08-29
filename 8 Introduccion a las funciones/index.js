3b5975f9303f3f748d09c7070476227d579b2d3a
claudio-olivera <claudioolivera753@gmail.com>
2022-08-29 15:25:56 -0300

    8. ejercicio

--- 8 Introduccion a las funciones/index.js
+++ 8 Introduccion a las funciones/index.js
@@ -6,10 +6,19 @@ async function damePromesa() {
     return setTimeout(() => console.log("Hola soy una promesa"), 5000)
 }
 
-function* idsPares() {
-    let id = 0
-    while(true) {
-        yield id += 2
+function* indicesParesAutomaticos(){
+    let i = 1;
+    while (true){
+        i *= 2 
+        if(i > 32){
+            return
+        }
+        yield i
     }
 }
- 
+generador = indicesParesAutomaticos();
+console.log(generador.next().value)
+console.log(generador.next().value)
+console.log(generador.next().value)
+console.log(generador.next().value)
+console.log(generador.next().value)
