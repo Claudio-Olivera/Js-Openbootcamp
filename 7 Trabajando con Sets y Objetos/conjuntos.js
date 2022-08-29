2b0114855dc6fdc44878f84c7859c51f5d1952ca
claudio-olivera <claudioolivera753@gmail.com>
2022-08-28 22:35:00 -0300

    7. ejercicio 1

---
+++ 7 Trabajando con Sets y Objetos/conjuntos.js
@@ -0,0 +1,12 @@
+const nombres=["raul","jimena","patricio","miguel"] 
+const nombresFamilia = new Set(nombres)
+console.log(nombresFamilia)
+
+console.log(nombres)
+console.log(nombresFamilia)
+
+nombresFamilia.add("claudio")
+console.log(nombresFamilia)
+
+nombresFamilia.add("javascript")
+console.log(nombresFamilia)
