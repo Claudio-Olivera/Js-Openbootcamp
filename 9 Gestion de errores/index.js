4fcdf22ebb29334faa0d7f354ad6952eabaea38f
claudio-olivera <claudioolivera753@gmail.com>
2022-08-29 16:34:15 -0300

    9.Gestion errores  ejercicio

---
+++ 9 Gestion de errores/index.js
@@ -0,0 +1,8 @@
+const logger = require('./winston')
+
+// logger.log("Hola estoy saliendo por pantalla")
+logger.info("Hola esto es un mensaje informativo")
+logger.debug("Esto es un mensaje de debug")
+logger.warn("Esto es un mensaje de advertencia")
+logger.error("Esto es un error")
+
