2dba6d7a8981b2b784f34f014df8486911c76685
claudio-olivera <claudioolivera753@gmail.com>
2022-08-29 18:12:23 -0300

    10. Ejercicio import export chalk lib

---
+++ 10 Modulos en JavaScript/controller.js
@@ -0,0 +1,10 @@
+
+export function suma(a, b){
+    return a + b
+}
+
+function multiplicar(a, b){
+    return a * b
+}
+
+export default multiplicar;
---
+++ 10 Modulos en JavaScript/index.js
@@ -0,0 +1,6 @@
+import multiplicar,{suma} from "./controller.js"
+import chalk from 'chalk';
+
+
+console.log(suma(1,2))
+console.log(chalk.green(multiplicar(4,5)));
